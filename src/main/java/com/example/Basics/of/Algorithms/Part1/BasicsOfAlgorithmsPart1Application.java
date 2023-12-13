ed7cfafa68a5f029330fcdfb7aecf92d95ffa39d
khilola.kushbakova <xilola_kushbakova@mai.ru>
2023-12-13 21:33:02 +0200

    initial

---
+++ src/main/java/com/example/Basics/of/Algorithms/Part1/BasicsOfAlgorithmsPart1Application.java
@@ -0,0 +1,13 @@
+package com.example.Basics.of.Algorithms.Part1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicsOfAlgorithmsPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicsOfAlgorithmsPart1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Basics/of/Algorithms/Part1/BasicsOfAlgorithmsPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Basics.of.Algorithms.Part1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicsOfAlgorithmsPart1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
